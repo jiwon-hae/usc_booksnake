f10d7b85d5b5ecf7b7509538b8a749fb1920823f
Jiwon Hae <hjiwon1994@gmail.com>
2024-01-03 15:37:22 -0800

    feat : Implement Lists usecase and repository

---
+++ app/src/main/java/com/usc/booksnake/data/remote/ListsRemoteSource.kt
@@ -0,0 +1,9 @@
+package com.usc.booksnake.data.remote
+
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class ListsRemoteSource @Inject constructor(){
+
+}
---
+++ app/src/main/java/com/usc/booksnake/domain/repository/ListsRepository.kt
@@ -0,0 +1,4 @@
+package com.usc.booksnake.domain.repository
+
+interface ListsRepository {
+}
